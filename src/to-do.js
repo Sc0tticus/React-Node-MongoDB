529b16af09c50e84999fcd12ee29a2117f114a8f
Sc0tticus <ssinger303@gmail.com>
2021-10-04 20:55:06 -0600

    CH10 6S

--- src/to-do.js
+++ src/to-do.js
@@ -4,20 +4,41 @@ class TODO extends React.Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-			text: ''
+			text: '',
+			items: []
 		};
 		this.handleChange = this.handleChange.bind(this);
+		this.handleSubmit = this.handleSubmit.bind(this);
 	}
 
 	handleChange(e) {
 		this.setState({ text: e.target.value });
+	}
+	handleSubmit(e) {
+		e.preventDefault();
+		if (this.state.text.length === 0) return;
+
+		const newItem = {
+			text: this.state.text,
+			id: Date.now()
+		};
+		this.setState(state => ({
+			items: state.items.concat(newItem),
+			text: ''
+		}));
 	}
 
 	render() {
 		return (
 			<div>
 				<h1>TODO APP</h1>
-				<form>
+				<ul>
+					{this.state.items.map(item => (
+						<li key={item.id}>{item.text}</li>
+					))}
+				</ul>
+
+				<form onSubmit={this.handleSubmit}>
 					<label htmlFor="id">Input Item</label>
 					<br />
 					<label>{this.state.text}</label>
